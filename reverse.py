3eca27519712e4998df8e6f9ab32ed128c226e22
Francis Githae <mureithigithae@gmail.com>
2022-10-18 16:36:38 +0300

    ft(reverse string):add method to reverse a string

---
+++ reverse.py
@@ -0,0 +1,12 @@
+def reverse_string(string):
+    """
+    This function reverses a string 
+
+    Parameters: input string
+
+    Returns: Reversed string
+    """
+
+    return string[::-1]
+
+print(reverse_string("Good morning"))
